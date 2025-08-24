f62ea25cc9d3ef3eacdf5df2d7e5a5f367d161d4
mad-dev-27 <arttimevideo@gmail.com>
2025-08-24 22:18:09 +0530

    Fix Dashboard Layout and Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ const Dashboard: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="h-screen bg-gray-50 dark:bg-gray-900 flex flex-col overflow-hidden">
       <Header />
-      <div className="flex flex-col lg:flex-row min-h-screen">
+      <div className="flex flex-col lg:flex-row flex-1 overflow-hidden">
         <Sidebar {...sidebarData} />
         <DashboardContent />
       </div>
