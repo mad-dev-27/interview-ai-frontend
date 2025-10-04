fa6af8738660f6c35f6fd26db99f0705076c324c
mad-dev-27 <arttimevideo@gmail.com>
2025-10-04 18:43:25 +0530

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://api.jobprepai.in";
+// export const API_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000"
+//     : "https://api.jobprepai.in";
 
+export const API_URL = "https://api.jobprepai.in";
